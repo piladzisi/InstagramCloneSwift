02a84b42407a366d2aef5511617af03fa5ce1e85
piladzisi <piladzisi@gmail.com>
2019-06-29 16:10:42 +0300

    initial commit

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by anna.sibirtseva on 29/06/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
